e9041d251073fef42bd1119104c93d174554be12
Alex <alexandr.jelia@gmail.com>
2022-09-06 01:09:46 +0300

    init

---
+++ src/alex/jelia/empmanager/Resume.java
@@ -0,0 +1,15 @@
+package alex.jelia.empmanager;
+
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    // Unique identifier
+    String uuid;
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
