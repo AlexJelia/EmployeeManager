a9d0c100a71c03ce4cda3fde863b681a324b67b1
Alex <alexandr.jelia@gmail.com>
2022-10-05 17:34:42 +0300

    MapResumeStorageTest added

---
+++ test/alex/jelia/empmanager/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,8 @@
+package alex.jelia.empmanager.webapp.storage;
+
+public class MapResumeStorageTest extends AbstractStorageTest{
+
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
